fc0d6d464d181e1c130bdf68ce1ffabf670a94a0
Petteri Mattila <petterimattila@protonmail.com>
2023-12-12 12:07:00 +0200

    Tidying the tensor(s)

--- chapter3/extra/tic-tac-toe/main.js
+++ chapter3/extra/tic-tac-toe/main.js
@@ -12,7 +12,12 @@ const boardState = [
     [1, 0, 0],
 ]
 
-const result = tf.tensor2d(boardState, null, "int32")
+const tensor = tf.tensor2d(boardState, null, "int32")
 
-gc.innerHTML = `<p>${ JSON.stringify(result) }</p>`
-console.log("TF result", result)
+gc.innerHTML = `<p>${ JSON.stringify(tensor) }</p>`
+
+console.log("Checking before tidy/dispose:", tf.memory().numTensors, tf.memory().numBytes)
+
+tensor.dispose()
+
+console.log("Checking after manual cleaning:", tf.memory().numTensors, tf.memory().numBytes)
