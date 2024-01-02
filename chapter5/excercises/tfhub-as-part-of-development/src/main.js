0ce5b9e2a9856bd474fcd37e4d080c1bfe3113f1
Petteri Mattila <petterimattila@protonmail.com>
2024-01-02 15:54:09 +0200

    Object cassification exercise

---
+++ chapter5/excercises/tfhub-as-part-of-development/src/main.js
@@ -0,0 +1,75 @@
+import * as tf from "@tensorflow/tfjs"
+import { INCEPTION_CLASSES } from "./labels"
+
+tf.ready().then(() => {
+    const predicts = document.querySelectorAll(".prediction")
+    const processing = document.getElementById("processing")
+    processing.innerHTML = "Processing . . ."
+
+    const modelPath = "https://www.kaggle.com/models/google/inception-v3/frameworks/TfJs/variations/classification/versions/2"
+
+    tf.tidy(() => {
+        tf.loadGraphModel(modelPath, { fromTFHub: true })
+            .then(model => {
+                const imageElements = document.querySelectorAll(".image")
+                const images = []
+                imageElements.forEach(image => images.push(image))
+
+                processing.innerHTML = "Creating tensors . . ."
+                const tensors = images.map(image =>
+                    tf.browser.fromPixels(image)
+                )
+
+                // Resizing to match what model expects, which is 299x299
+                processing.innerHTML = "Reshaping tensor(s)"
+                const resizedImageTensors = tensors.map(t =>
+                    tf.image
+                        .resizeBilinear(t, [299, 299], true)
+                        .div(255)
+                        // For some reason the reshape is unable to convert the image to expected dimensions at this moment.
+                        // .reshape(1, 299, 299, 3)
+                        // Which is why reshape is replaced with expandDims.
+                        .expandDims(0)
+                )
+
+                console.log("resizedImageTensors", resizedImageTensors)
+
+                const results = resizedImageTensors.map(resizedT => {
+                    const r = model.predict(resizedT)
+                    r.print()
+                    return r
+                })
+
+
+                const indices = results.map(result => {
+                    const { values, indices } = tf.topk(result, 3)
+                    indices.print()
+                    return indices
+                })
+
+                const predictionList = indices.map(indice => indice.dataSync())
+                const listedPredictions = predictionList.map((predictions, i) => {
+                    const imagePredicts = predictions.map((p, ii) => {
+                        // Because the model doesn't include labels, the program needs to match the prediction with the labels.
+                        const label = INCEPTION_CLASSES[p]
+                        console.log(`Prediction (${ii + 1}, image ${i + 1}):`, label)
+
+                        if (predicts[i].innerHTML === "")
+                            predicts[i].innerHTML = label
+                        else predicts[i].innerHTML += ", " + label
+
+                        return label
+                    })
+
+                    return imagePredicts
+                })
+                console.log(listedPredictions)
+
+                processing.innerHTML = ""
+            })
+            .catch(err => {
+                console.error("Error occured with model: " + modelPath + "'.\n", err)
+                processing.innerHTML = err
+            })
+    })
+})
