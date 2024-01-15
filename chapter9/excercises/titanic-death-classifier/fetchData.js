3c60ef6590c2883378980f72231e8b8949fc6f99
Petteri Mattila <petterimattila@protonmail.com>
2024-01-15 16:08:46 +0200

    Chapter 9, part 1 Titanic survivability

---
+++ chapter9/excercises/titanic-death-classifier/fetchData.js
@@ -0,0 +1,103 @@
+import * as dfd from "danfojs-node"
+
+export async function fetchTitanicData() {
+    try {
+        // Check if clean-data directory has the files before creating them
+        const dfTrain = await dfd.readCSV("./clean-data/cleanTest.csv") || null
+        const dfTest = await dfd.readCSV("./clean-data/cleanTrain.csv") || null
+        
+        return [dfTest, dfTrain]
+    } catch (error) {
+        console.log("fetchData.js fetchTitanicData:", error)
+        // Otherwise fetch and create datasets
+        // If danfo.js is unable to fetch files, it will prompt an error
+        const datasetSamples = createUsableTitanicDatasets()
+        return datasetSamples
+    }
+}
+
+async function createUsableTitanicDatasets() {
+    const df = await dfd.readCSV("../../extra/titanic data/train.csv")
+    const dft = await dfd.readCSV("../../extra/titanic data/test.csv")
+
+    console.log("Train size", df.shape[0])
+    console.log("Test size", dft.shape[0])
+
+    // Combining file contents so the empty value check can be done once
+    const full = dfd.concat({ dfList: [df, dft], axis: 0 })
+    // full.describe().print()
+
+    const cleanFullList = cleanDataset(full)
+    const encodedFullList = encodeDatasetColumns(cleanFullList, ["Embarked", "Sex"])
+    const separatedLists = createSampleDatasets(encodedFullList, true)
+    return separatedLists
+}
+
+function cleanDataset(data) {
+    console.log("Row-count", data.shape[0])
+    const emptySpots = data.isNa().sum()
+    const emptyRate = emptySpots.div(data.isNa().count())
+    // console.log("Empty rate (Going to be cleaned)")
+    // emptyRate.print()
+    
+    const clean = data.drop({
+        columns: ["Name", "PassengerId", "Cabin", "Ticket"]
+    })
+    
+    // DropNa() should make sure the end product is complete record of a passenger without missing data.
+    const intactDataSet = clean.dropNa()
+    console.log("After cleaning the row-count is now", intactDataSet.shape[0])
+    
+    return intactDataSet
+}
+
+function encodeDatasetColumns(data, columnNames) {
+    if (typeof columnNames !== "object") {
+        console.log("EncodeDataseColumns-function: Please pass an array as the secondary argument.")
+        return
+    }
+
+    columnNames.forEach(colN => {
+        const encode = new dfd.LabelEncoder()
+        const targetColumn = data[colN]
+        encode.fit(targetColumn)
+        data[colN] = encode.transform(targetColumn.values)
+    })
+
+    // Uncomment if visual check is necessary.
+    // data.head().print()
+
+    return data
+}
+
+async function createSampleDatasets(data, createSampleFiles = false) {
+    let datasets = []
+
+    // For some reason sample() breaks the application when trying to pass bigger numbers than 420.
+    // This is caused because the data doesn't include over 420 rows (which is false, we have over 1000 records),
+    // which is why we create test samples first and the remanining is used for training.
+    // Ofcourse there is some unwanted results, which is the sample() shuffles the result, which could be useful for training dataset.
+    const newTestingDataset = await data.sample(200)
+    console.log("New testing dataset row count:", newTestingDataset.shape[0])
+    datasets.push(newTestingDataset)
+
+    const newTrainingDataset = data.drop({ index: newTestingDataset.index, axis: 0 })
+    console.log("New training dataset row count:", newTrainingDataset.shape[0])
+    datasets.push(newTrainingDataset)
+
+    if (createSampleFiles) {
+        createFile(datasets[0], { path: "./clean-data/cleanTest.csv" })
+        createFile(datasets[1], { path: "./clean-data/cleanTrain.csv" })
+    }
+
+    return datasets
+}
+
+async function createFile(data, fileDetails) {
+    try {
+        dfd.toCSV(data, { filePath: fileDetails.path })
+        console.log("File created:", fileDetails.path)
+    } catch (error) {
+        console.warn("FetchData.js, createFile-function. Be sure to create 'clean-data' directory in the root.", error)
+    }
+}
---
+++ chapter9/excercises/titanic-death-classifier/index.js
@@ -0,0 +1,29 @@
+import * as tf from "@tensorflow/tfjs-node" 
+import { fetchTitanicData } from "./fetchData.js"
+import createTfModel from "./createModel.js"
+
+async function trainModel() {
+    const [trainingDataset, testDataset] = await fetchTitanicData()
+    console.log("============ Datasets fetched ============")
+    console.log("Testing dataset", testDataset.shape[0])
+    console.log("Training dataset", trainingDataset.shape[0])
+
+    // Create X tensor that contains enough variables to indicate if the person survived, excluding the result
+    const trainX = trainingDataset.iloc({ columns: ["1:"] }).tensor
+    // Contains the answer if the person survived.
+    const trainY = trainingDataset["Survived"].tensor
+
+    const testX = testDataset.iloc({ columns: ["1:"] }).tensor
+    const testY = testDataset["Survived"].tensor
+
+    const model = createTfModel(tf, [trainX.shape[1]])
+
+    await model.fit(trainX, trainY, {
+        batchSize: 32,
+        epochs: 150,
+        // validationData: [testX, testY]
+        validationSplit: 0.2
+    })
+}
+
+trainModel()
